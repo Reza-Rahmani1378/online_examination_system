c38d6260fc858e640e1b6d280b024575d46a1003
Reza-Rahmani1378 <reza6273757@gmail.com>
2022-04-06 00:45:39 +0430

    Add UserRepository

---
+++ online_styem/src/main/java/com/tosan/online_styem/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.tosan.online_styem.repositories;
+
+import com.tosan.online_styem.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}

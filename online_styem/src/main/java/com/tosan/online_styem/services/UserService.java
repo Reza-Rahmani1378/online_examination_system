64192c7d41ce4475c592a41086f2b30206066b11
Reza-Rahmani1378 <reza6273757@gmail.com>
2022-04-06 00:59:16 +0430

    Add UserService

---
+++ online_styem/src/main/java/com/tosan/online_styem/services/UserService.java
@@ -0,0 +1,7 @@
+package com.tosan.online_styem.services;
+
+import com.tosan.online_styem.base.service.BaseService;
+import com.tosan.online_styem.models.User;
+
+public interface UserService extends BaseService<User, Long> {
+}
---
+++ online_styem/src/main/java/com/tosan/online_styem/services/impl/UserServiceImpl.java
@@ -0,0 +1,49 @@
+package com.tosan.online_styem.services.impl;
+
+import com.tosan.online_styem.base.service.impl.BaseServiceImpl;
+import com.tosan.online_styem.exception.AccessDeniedRunTimeException;
+import com.tosan.online_styem.models.User;
+import com.tosan.online_styem.repositories.UserRepository;
+import com.tosan.online_styem.services.UserService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl extends BaseServiceImpl<User, Long, UserRepository> implements UserService {
+
+    public UserServiceImpl(UserRepository repository) {
+        super(repository);
+    }
+
+    @Override
+    @Transactional
+    public User saveNotSecure(User user) {
+        return super.saveNotSecure(user);
+    }
+
+    @Override
+    @Transactional
+    public List<User> saveAllNotSecure(Collection<User> collection) {
+        return super.saveAllNotSecure(collection);
+    }
+
+    @Override
+    public Optional<User> findByIdNotSecure(Long id) {
+        return super.findByIdNotSecure(id);
+    }
+
+    @Override
+    public List<User> findAllNotSecure() {
+        return super.findAllNotSecure();
+    }
+
+    @Override
+    public void deleteByIdNotSecure(Long id) {
+        super.deleteByIdNotSecure(id);
+        throw new AccessDeniedRunTimeException("can't delete entity with id " + id);
+    }
+}

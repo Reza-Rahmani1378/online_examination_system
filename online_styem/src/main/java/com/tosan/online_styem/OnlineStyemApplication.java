e1c15cedf34254c163565a50ebae9e0e30379294
Reza-Rahmani1378 <reza6273757@gmail.com>
2022-04-06 01:49:45 +0430

    Refactor OnlineStyemApplication

--- online_styem/src/main/java/com/tosan/online_styem/OnlineStyemApplication.java
+++ online_styem/src/main/java/com/tosan/online_styem/OnlineStyemApplication.java
@@ -2,9 +2,16 @@ package com.tosan.online_styem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class OnlineStyemApplication {
+public class OnlineStyemApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(OnlineStyemApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(OnlineStyemApplication.class, args);

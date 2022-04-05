5f404ef9fe6229eda416e7bb3b080e777efca5e0
Reza-Rahmani1378 <reza6273757@gmail.com>
2022-04-06 00:59:32 +0430

    Add StudentService

---
+++ online_styem/src/main/java/com/tosan/online_styem/services/StudentService.java
@@ -0,0 +1,7 @@
+package com.tosan.online_styem.services;
+
+import com.tosan.online_styem.base.service.BaseService;
+import com.tosan.online_styem.models.Student;
+
+public interface StudentService extends BaseService<Student, Long> {
+}
---
+++ online_styem/src/main/java/com/tosan/online_styem/services/impl/StudentServiceImpl.java
@@ -0,0 +1,49 @@
+package com.tosan.online_styem.services.impl;
+
+import com.tosan.online_styem.base.service.impl.BaseServiceImpl;
+import com.tosan.online_styem.exception.AccessDeniedRunTimeException;
+import com.tosan.online_styem.models.Student;
+import com.tosan.online_styem.repositories.StudentRepository;
+import com.tosan.online_styem.services.StudentService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class StudentServiceImpl extends BaseServiceImpl<Student, Long, StudentRepository> implements StudentService {
+
+    public StudentServiceImpl(StudentRepository repository) {
+        super(repository);
+    }
+
+    @Override
+    @Transactional
+    public Student saveNotSecure(Student student) {
+        return super.saveNotSecure(student);
+    }
+
+    @Override
+    @Transactional
+    public List<Student> saveAllNotSecure(Collection<Student> collection) {
+        return super.saveAllNotSecure(collection);
+    }
+
+    @Override
+    public Optional<Student> findByIdNotSecure(Long id) {
+        return super.findByIdNotSecure(id);
+    }
+
+    @Override
+    public List<Student> findAllNotSecure() {
+        return super.findAllNotSecure();
+    }
+
+    @Override
+    public void deleteByIdNotSecure(Long id) {
+        super.deleteByIdNotSecure(id);
+        throw new AccessDeniedRunTimeException("can't delete entity with id " + id);
+    }
+}

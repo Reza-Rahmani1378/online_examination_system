bd017bc715b9083fa1d3efc9f911cce1eae22bbe
Reza-Rahmani1378 <reza6273757@gmail.com>
2022-04-06 00:06:39 +0430

    Refactor UserEntity

--- online_styem/src/main/java/com/tosan/online_styem/models/User.java
+++ online_styem/src/main/java/com/tosan/online_styem/models/User.java
@@ -18,22 +18,25 @@ public class User extends BaseEntity<Long> {
     public static final String TABLE_NAME = "user_table";
     private static final String FIRST_NAME = "first_name";
     private static final String LAST_NAME = "last_name";
+    private static final String NATIONAL_CODE = "national_code";
+    private static final String USER_TYPE = "user_type";
 
 
+    @Column(name = User.FIRST_NAME)
     private String firstName;
-
+    @Column(name = User.LAST_NAME)
     private String lastName;
 
     @Column(unique = true)
     private String username;
 
     private String password;
-
+    @Column(name = User.NATIONAL_CODE)
     private String nationalCode;
 
     private String email;
 
-    @Column()
+    @Column(name = User.USER_TYPE)
     @Enumerated(EnumType.STRING)
     private UserType userType;
 
@@ -41,21 +44,7 @@ public class User extends BaseEntity<Long> {
     private boolean isConfirmed;
 
 
-    public User(String firstName,
-                String lastName,
-                String username,
-                String password,
-                String nationalCode,
-                String email,
-                boolean isConfirmed) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.username = username;
-        this.password = password;
-        this.nationalCode = nationalCode;
-        this.email = email;
-        this.isConfirmed = isConfirmed;
-    }
+
 
     @Override
     public String toString() {
@@ -68,9 +57,6 @@ public class User extends BaseEntity<Long> {
                 ", isConfirmed=" + isConfirmed +
                 '}';
     }
-
-
-
 
 
 }

a16776c48046ba44fe464d721b25f24158f58a62
Reza-Rahmani1378 <reza6273757@gmail.com>
2022-04-06 00:11:21 +0430

    Add BadInputRunTimeException for Exception Handling.

--- online_styem/src/main/java/com/tosan/online_styem/exception/BadInputRunTimeException.java
+++ online_styem/src/main/java/com/tosan/online_styem/exception/BadInputRunTimeException.java
@@ -2,7 +2,7 @@ package com.tosan.online_styem.exception;
 
 public class BadInputRunTimeException extends RuntimeException {
 
-    public BadInputRunTimeException(String message) {
-        super(message);
+    public BadInputRunTimeException(String exception) {
+        super(exception);
     }
 }

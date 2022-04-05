ee5352a4fbb5edbcc443403f1632249b9edb0d45
Reza-Rahmani1378 <reza6273757@gmail.com>
2022-04-06 01:46:04 +0430

    Refactor Swagger

--- online_styem/src/main/java/com/tosan/online_styem/config/SwaggerConfig.java
+++ online_styem/src/main/java/com/tosan/online_styem/config/SwaggerConfig.java
@@ -5,10 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebFlux;
 
 @Configuration
-@EnableSwagger2WebMvc
+@EnableSwagger2WebFlux
 public class SwaggerConfig {
 
     @Bean

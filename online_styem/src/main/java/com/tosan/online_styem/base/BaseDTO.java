bc9dbe6842bacb4dc952d724e0cfadb3d1067dec
Reza-Rahmani1378 <reza6273757@gmail.com>
2022-04-06 00:28:55 +0430

    Refactor BaseDTO and BaseDTO

--- online_styem/src/main/java/com/tosan/online_styem/base/BaseDTO.java
+++ online_styem/src/main/java/com/tosan/online_styem/base/BaseDTO.java
@@ -9,5 +9,5 @@ import java.io.Serializable;
 @Setter
 public class BaseDTO <PK extends Serializable> implements Serializable{
 
-    private PK id;
+    protected PK id;
 }
--- online_styem/src/main/java/com/tosan/online_styem/services/dto/UserDTO.java
+++ online_styem/src/main/java/com/tosan/online_styem/services/dto/UserDTO.java
@@ -13,7 +13,7 @@ import javax.persistence.Enumerated;
 @Getter
 @Setter
 public class UserDTO extends BaseDTO<Long> {
-    private Long id;
+
 
     @JsonProperty("firstName")
     private String firstName;

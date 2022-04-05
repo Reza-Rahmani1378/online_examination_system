6800bb3e05484ec9f4eb26320cf73bd61dd843fc
Reza-Rahmani1378 <reza6273757@gmail.com>
2022-04-06 00:27:51 +0430

    Add UserDTO

---
+++ online_styem/src/main/java/com/tosan/online_styem/services/dto/UserDTO.java
@@ -0,0 +1,45 @@
+package com.tosan.online_styem.services.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.sun.istack.NotNull;
+import com.tosan.online_styem.models.enumeration.UserType;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+
+@Getter
+@Setter
+public class UserDTO {
+    private Long id;
+
+    @JsonProperty("firstName")
+    private String firstName;
+
+    @JsonProperty("lastName")
+    private String lastName;
+
+    @NotNull
+    @JsonProperty("username")
+    private String username;
+
+    @JsonProperty("nationalCode")
+    private String nationalCode;
+
+
+    @NotNull
+    @JsonProperty("password")
+    private String password;
+
+    @NotNull
+    @JsonProperty("email")
+    private String email;
+
+    @NotNull
+    @JsonProperty("userType")
+    @Enumerated(EnumType.STRING)
+    private UserType userType;
+
+    private boolean isConfirmed;
+}

262be925f644bc5bac334c13a71896ce7609c270
Reza-Rahmani1378 <reza6273757@gmail.com>
2022-04-06 00:45:24 +0430

    Add StudentEntity.

---
+++ online_styem/src/main/java/com/tosan/online_styem/models/Student.java
@@ -0,0 +1,29 @@
+package com.tosan.online_styem.models;
+
+import com.tosan.online_styem.models.enumeration.UserType;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table
+@Getter
+@Setter
+@NoArgsConstructor
+public class Student extends User {
+    public static final String TABLE_NAME = "student_table";
+
+
+    public Student(String firstName,
+                   String lastName,
+                   String username,
+                   String password,
+                   String nationalCode,
+                   String email,
+                   boolean isConfirmed) {
+        super(firstName, lastName, username, password, nationalCode, email, UserType.STUDENT, isConfirmed);
+    }
+}

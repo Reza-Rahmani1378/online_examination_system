c1cf17e21dca6b0277896d55c7f64f280ea14339
Reza-Rahmani1378 <reza6273757@gmail.com>
2022-04-06 00:57:28 +0430

    Add AccessDeniedRunTimeException

---
+++ online_styem/src/main/java/com/tosan/online_styem/exception/AccessDeniedRunTimeException.java
@@ -0,0 +1,7 @@
+package com.tosan.online_styem.exception;
+
+public class AccessDeniedRunTimeException extends RuntimeException {
+    public AccessDeniedRunTimeException(String message) {
+        super(message);
+    }
+}

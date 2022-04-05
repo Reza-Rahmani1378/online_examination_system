dc7973efc874b1d880bf7652b18a5c00ef5df7f1
Reza-Rahmani1378 <reza6273757@gmail.com>
2022-04-05 16:42:43 +0430

    Add Necessary files for project

---
+++ online_styem/src/main/java/com/tosan/online_styem/OnlineStyemApplication.java
@@ -0,0 +1,13 @@
+package com.tosan.online_styem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineStyemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineStyemApplication.class, args);
+	}
+
+}
---
+++ online_styem/src/test/java/com/tosan/online_styem/OnlineStyemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tosan.online_styem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineStyemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

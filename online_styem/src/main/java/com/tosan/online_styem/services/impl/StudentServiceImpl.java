bf5f57742c4678cc06ae289131ed7118d095b86e
Reza-Rahmani1378 <reza6273757@gmail.com>
2022-04-07 03:55:44 +0430

    Refactor StudentServiceImpl

--- online_styem/src/main/java/com/tosan/online_styem/services/impl/StudentServiceImpl.java
+++ online_styem/src/main/java/com/tosan/online_styem/services/impl/StudentServiceImpl.java
@@ -43,7 +43,12 @@ public class StudentServiceImpl extends BaseServiceImpl<Student, Long, StudentRe
 
     @Override
     public void deleteByIdNotSecure(Long id) {
-        super.deleteByIdNotSecure(id);
-        throw new AccessDeniedRunTimeException("can't delete entity with id " + id);
+        try {
+            super.deleteByIdNotSecure(id);
+
+        } catch (Exception e) {
+            throw new AccessDeniedRunTimeException("can't delete entity with id " + id);
+
+        }
     }
 }

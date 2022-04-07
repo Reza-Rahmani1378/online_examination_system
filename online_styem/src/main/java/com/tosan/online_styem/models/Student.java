add223e3449c2e93d74e1b4da06393cc941d63d3
Reza-Rahmani1378 <reza6273757@gmail.com>
2022-04-07 18:58:56 +0430

    Refactor StudentEntity

--- online_styem/src/main/java/com/tosan/online_styem/models/Student.java
+++ online_styem/src/main/java/com/tosan/online_styem/models/Student.java
@@ -1,7 +1,6 @@
 package com.tosan.online_styem.models;
 
 import com.tosan.online_styem.models.enumeration.UserType;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -14,7 +13,7 @@ import javax.persistence.Table;
 @Getter
 @Setter
 @NoArgsConstructor
-@Builder
+
 public class Student extends User {
     public static final String TABLE_NAME = "student_table";
 

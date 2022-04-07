8fd595b2f4112326b900d648f2432a684a635754
Reza-Rahmani1378 <reza6273757@gmail.com>
2022-04-07 18:00:37 +0430

    Add TokenRepository

---
+++ online_styem/src/main/java/com/tosan/online_styem/repositories/TokenRepository.java
@@ -0,0 +1,9 @@
+package com.tosan.online_styem.repositories;
+
+import com.tosan.online_styem.models.Token;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TokenRepository extends JpaRepository<Token, Long> {
+}

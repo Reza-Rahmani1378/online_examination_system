19256158e6f7dae470ef099ef06b066fabc97300
Reza-Rahmani1378 <reza6273757@gmail.com>
2022-04-07 18:04:22 +0430

    Refactor TokenRepository

--- online_styem/src/main/java/com/tosan/online_styem/repositories/TokenRepository.java
+++ online_styem/src/main/java/com/tosan/online_styem/repositories/TokenRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TokenRepository extends JpaRepository<Token, Long> {
+public interface TokenRepository extends JpaRepository<Token, Integer> {
 }

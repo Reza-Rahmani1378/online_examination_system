f06272ca79caaef7d83e7b89b5d43bc04fcfed8a
Reza-Rahmani1378 <reza6273757@gmail.com>
2022-04-07 18:33:32 +0430

    Add updateToken method in TokenRepository

--- online_styem/src/main/java/com/tosan/online_styem/repositories/TokenRepository.java
+++ online_styem/src/main/java/com/tosan/online_styem/repositories/TokenRepository.java
@@ -2,10 +2,17 @@ package com.tosan.online_styem.repositories;
 
 import com.tosan.online_styem.models.Token;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TokenRepository extends JpaRepository<Token, Integer> {
 
     Token getTokenByEmailId(String emailId);
+
+    @Modifying
+    @Query("update Token t set t.authenticationToken = :auth , t.secretKey = :secret where t.emailId = :email")
+    int updateToken(@Param("email") String email, @Param("auth") String authenticationToken, @Param("secret") String secretKey);
 }

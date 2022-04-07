5c34b8ae394ae2bc0166c33f30e338604994d683
Reza-Rahmani1378 <reza6273757@gmail.com>
2022-04-07 19:21:56 +0430

    Refactor TokenDTO

--- online_styem/src/main/java/com/tosan/online_styem/services/dto/TokenDTO.java
+++ online_styem/src/main/java/com/tosan/online_styem/services/dto/TokenDTO.java
@@ -1,16 +1,18 @@
 package com.tosan.online_styem.services.dto;
 
 import com.tosan.online_styem.base.BaseDTO;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 public class TokenDTO extends BaseDTO<Integer> {
 
     private Integer id;
 
-    private int userId;
+    private Long userId;
 
     private String emailId;
 }

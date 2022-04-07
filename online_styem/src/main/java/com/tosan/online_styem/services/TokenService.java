060f2364f630571ef4eed7a2509ccacda47bf717
Reza-Rahmani1378 <reza6273757@gmail.com>
2022-04-07 18:03:49 +0430

    Add TokenService

---
+++ online_styem/src/main/java/com/tosan/online_styem/services/TokenService.java
@@ -0,0 +1,7 @@
+package com.tosan.online_styem.services;
+
+import com.tosan.online_styem.base.service.BaseService;
+import com.tosan.online_styem.models.Token;
+
+public interface TokenService extends BaseService<Token, Integer> {
+}
---
+++ online_styem/src/main/java/com/tosan/online_styem/services/impl/TokenServiceImpl.java
@@ -0,0 +1,54 @@
+package com.tosan.online_styem.services.impl;
+
+import com.tosan.online_styem.base.service.impl.BaseServiceImpl;
+import com.tosan.online_styem.exception.AccessDeniedRunTimeException;
+import com.tosan.online_styem.models.Token;
+import com.tosan.online_styem.repositories.TokenRepository;
+import com.tosan.online_styem.services.TokenService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TokenServiceImpl extends BaseServiceImpl<Token, Integer, TokenRepository> implements TokenService {
+
+    public TokenServiceImpl(TokenRepository repository) {
+        super(repository);
+    }
+
+    @Override
+    @Transactional
+    public Token saveNotSecure(Token student) {
+        return super.saveNotSecure(student);
+    }
+
+    @Override
+    @Transactional
+    public List<Token> saveAllNotSecure(Collection<Token> collection) {
+        return super.saveAllNotSecure(collection);
+    }
+
+    @Override
+    public Optional<Token> findByIdNotSecure(Integer id) {
+        return super.findByIdNotSecure(id);
+    }
+
+    @Override
+    public List<Token> findAllNotSecure() {
+        return super.findAllNotSecure();
+    }
+
+    @Override
+    public void deleteByIdNotSecure(Integer id) {
+        try {
+            super.deleteByIdNotSecure(id);
+
+        } catch (Exception e) {
+            throw new AccessDeniedRunTimeException("can't delete entity with id " + id);
+
+        }
+    }
+}

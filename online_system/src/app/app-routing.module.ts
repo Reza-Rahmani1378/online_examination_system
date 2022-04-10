640bfc391aec9722e8c985a588f9bf4b0b689893
Reza-Rahmani1378 <reza6273757@gmail.com>
2022-04-10 23:03:51 +0430

    Add Pathes for Admin and Student And Professor.

--- online_system/src/app/app-routing.module.ts
+++ online_system/src/app/app-routing.module.ts
@@ -3,12 +3,18 @@ import {HomeComponent} from "./home/home.component";
 import {LoginComponent} from "./login/login.component";
 import {SignupComponent} from "./signup/signup.component";
 import {NgModule} from "@angular/core";
+import {AdminWorkbenchComponent} from "./admin/admin-workbench/admin-workbench.component";
+import {ProfessorWorkbenchComponent} from "./professor/professor-workbench/professor-workbench.component";
+import {StudentWorkbenchComponent} from "./student/student-workbench/student-workbench.component";
 
 const routes: Routes = [
 
   {path: '', component: HomeComponent},
   {path: 'login', component: LoginComponent},
   {path: 'signup', component: SignupComponent},
+  {path: 'adminWorkBench', component: AdminWorkbenchComponent},
+  {path: 'professorWorkBench', component: ProfessorWorkbenchComponent},
+  {path: 'studentWorkBench', component: StudentWorkbenchComponent}
 ]
 
 
